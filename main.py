407f4dc8c070c9647f0d3ee8d2cc205a7781cbf0
lclopes <lucascastro@id.uff.br>
2019-11-08 11:58:57 -0200

    Gauss algorithm working

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+from partial_pivoting import partialPivoting
+
+    
+#test matrix
+testA = [[2.0, 1.0, 3.0],
+         [4.0, 4.0, 3.0],
+         [6.0, 7.0, 4.0]]
+
+#results matrix
+testB = [7, 21, 32]
+partialPivoting(testA, testB)
