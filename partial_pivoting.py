71abd3014039054165fa2edbccf97341c6b69ed0
Lucas Lopes <lucascastro@id.uff.br>
2019-11-09 10:35:56 -0200

    Generating L matrix

--- partial_pivoting.py
+++ partial_pivoting.py
@@ -7,18 +7,21 @@
 #                                      ^ 
 # A função retorna a matriz 'a' após a operação de pivoteamento parcial.
 def partialPivoting(a):
+    mList = []
+    L = []
+    U = a
 
-    for k in range(len(a) - 1):
+    for k in range(len(U) - 1):
         #pivô começa como o primeiro elemento da primeira linha
-        pivot = a[k][k]
+        pivot = U[k][k]
 
         #salvo a linha do pivo para realizar a operação de troca
         pivot_line = k
 
-        for i in range(k+1, len(a)):
+        for i in range(k+1, len(U)):
             #verifico se o pivô é o maior elemento da coluna
-            if a[i][k] > pivot:
-                pivot = a[i][k]
+            if U[i][k] > pivot:
+                pivot = U[i][k]
                 pivot_line = i
         
         #se o pivô for 0: saímos da iteração
@@ -28,16 +31,37 @@ def partialPivoting(a):
 
             #se a linha do pivô não for a linha atual: troco as linhas
             if pivot_line != k:
-                for j in range (len(a)):
-                    swap = a[k][j]
-                    a[k][j] = a[pivot_line][j]
-                    a[pivot_line][j] = swap
+                for j in range (len(U)):
+                    swap = U[k][j]
+                    U[k][j] = U[pivot_line][j]
+                    U[pivot_line][j] = swap
         
         #realizo operação de soma de linhas
-        for i in range (k+1, len(a)):
-            m = a[i][k] / a[k][k]
-            a[i][k] = 0
-            for j in range (k+1, len(a)):
-                a[i][j] = a[i][j] - m * a[k][j]
+        for i in range (k+1, len(U)):
+            m = U[i][k] / U[k][k]
+            U[i][k] = 0
+            mList.append(m)
+            for j in range (k+1, len(U)):
+                U[i][j] = U[i][j] - m * U[k][j]
+                
+    #geração da matriz L
+    for i in range (len(U)):
+        linha = newLine(len(U))
+        for j in range (len(U[i])):
+            if i == j:
+                linha[j] = 1
+            elif i < j:
+                linha[j] = 0
+            else:
+                linha[j] = mList[j]
+        L.append(linha) 
     
-    return a
+    print(mList)
+    print(U) 
+    print(L) 
+
+def newLine(n):
+    line = []
+    for i in range(n):
+        line.append(0)
+    return line    
